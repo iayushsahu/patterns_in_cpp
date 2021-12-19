bfa802eb5741fc2a8e105ecabc94ff295cd0f671
Ayush <87942664+iayushsahu@users.noreply.github.com>
2021-12-19 20:53:04 +0530

    Add files via upload

---
+++ pattern1.cpp
@@ -0,0 +1,21 @@
+// rectangle
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int row, col;
+    cin >> row >> col;
+
+    for (int i = 1; i <= row; i++)
+    {
+        for (int j = 1; j <= col; j++)
+        {
+            cout << " *";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
